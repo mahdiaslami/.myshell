efb811337b40523104ccb50f1bea02f4e3af33ef
mahdiaslami <mahdyaslami@gmail.com>
2025-09-28 09:19:34 +0330

    feat(docker): mount custom php.ini for configuration
    This allows developers to easily override PHP settings such as
    memory_limit,
    max_execution_time, etc., within the Docker environment without
    modifying
    the base image.

--- docker/_base.sh
+++ docker/_base.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 docker run -it \
   -v "$(pwd)":$(pwd) \
   -v "$HOME/.composer":$HOME/.composer \
+  -v "${SCRIPT_DIR}/custom-php.ini:/usr/local/etc/php/conf.d/custom-php.ini" \
   -e COMPOSER_HOME=$HOME/.composer \
   -e http_proxy="${http_proxy}" \
   -e https_proxy="${https_proxy}" \
